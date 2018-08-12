548e5358f978e63ae7703b50b17053eb3bb9a1dd
Paul Williams <paul@highaltitudearchery.com>
2018-08-11 18:49:25 -0600

    updated to include support for code 128b

--- barcode-encode.js
+++ barcode-encode.js
@@ -3,5 +3,5 @@
 const args = process.argv.slice(2);
 const barcode_encode = require('./lib/npm-barcode-encode.js');
 
-console.log(barcode_encode.Code128A(args[0]));
+console.log(barcode_encode.Code128B(args[0]));
 
